dcbb4f83e2286a5d15d60c3b511ccd8d8e583efb
Parth <parthbhandari17851@gmail.com>
2025-02-19 16:49:22 +0530

    18feb

---
+++ Middleware/18feb.js
@@ -0,0 +1,24 @@
+const express =require("express")
+const app=express()
+
+
+app.use(express.urlencoded({extended:true}))
+let userData=[]
+// converts urlencoded data into objects
+//it is a middleware that has to be run on every requests
+app.get("/adduser",(req,res)=>{
+    res.sendFile(__dirname+"/18feb.html")
+})
+
+
+app.post("/adduser",(req,res)=>{
+    let {name,email,password}= req.body;
+    userData.push({name:name,email:email,password:password})
+    console.log(name,email,password)
+    res.send(userData)
+})
+app.listen(22461,()=>{
+    console.log("server started")
+})
+
+// body------content type-->json, urlencoded, multi path form data-------converted into objects
